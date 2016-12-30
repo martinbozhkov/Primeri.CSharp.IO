37967cf5eb7874c48be6237aab8fd8ce274374ba
Мартин Божков <martin.bozhkov@mail.bg>
2016-12-30 12:15:03 +0200

    Модул 08, Видео 02 - Запис на текстов файл

---
+++ IOTesting/IOTextFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IOTextFiles
+{
+	class MainClass
+	{
+
+		public static void Main(string[] args)
+		{
+			STable _STable = new STable();
+			IOSettings _io = new IOSettings(_STable);
+
+			Console.WriteLine(_STable.stable[0]);
+
+			if (_io.save())
+			{
+				Console.WriteLine("\nТаблицата е запаметена успешно.\n");
+			}
+			else {
+				Console.WriteLine("\nТаблицата НЕ е запаметена успешно.\n");
+
+			}
+		}
+	}
+}
---
+++ IOTesting/IOTextFiles/STable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace IOTextFiles
+{
+	public class STable
+	{
+		public string[] stable = new string[4];
+
+		public STable()
+		{
+			fillStable();
+		}
+
+		private void fillStable()
+		{
+			stable[0] = "1,2,3,4,5";
+			stable[1] = "10,20,30,40,50";
+			stable[2] = "100,200,300,400,500";
+			stable[3] = "1000,2000,3000,4000,5000";
+
+		}
+
+	}
+}
